b7cc92834c3350ca136345c51a7eb9c5712a472f
Iván Chavero <ichavero@chavero.com.mx>
2024-09-06 12:33:07 -0600

    Added script for generating diesel models from current SQL

---
+++ generate_models.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DERIVE_FLAGS="Insertable, Queryable, Debug, Identifiable, Serialize, Deserialize"
+
+echo "Running diesel migrations"
+diesel migration run
+
+echo "Gettting schema from database"
+diesel print-schema > src/schema.rs
+
+echo "Generating models"
+diesel_ext -t -s src/schema.rs -m \
+  -I "crate::schema::*"           \
+  -d "${DERIVE_FLAGS}" > src/models.rs
+
+
