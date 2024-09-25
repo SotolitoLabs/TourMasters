c698d56612587d9467579aedb1e81db2866bc3df
Iván Chavero <ichavero@chavero.com.mx>
2024-09-24 22:47:35 -0600

    Remove all functions

--- migrations/2024-09-12-070904_users/down.sql
+++ migrations/2024-09-12-070904_users/down.sql
@@ -1,4 +1,6 @@
 -- This file should undo anything in `up.sql`
 
-DROP TABLE User;
+DROP TABLE Users;
 DROP FUNCTION IF EXISTS get_pw_hash;
+DROP FUNCTION IF EXISTS check_pw_hash;
+DROP FUNCTION IF EXISTS check_pw;
