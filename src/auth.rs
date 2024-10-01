3867b01492bb2ac32511a79b49f7487a5df93be1
Iván Chavero <ivan.chavero@ugd.gob.mx>
2024-09-30 23:57:15 -0600

    Fixed formatting

--- src/auth.rs
+++ src/auth.rs
@@ -1,10 +1,10 @@
+use crate::claims::Claims;
 /// Authentication functionalities
 use rocket::http::Status;
 use rocket::post;
 use rocket::response::status::Custom;
 use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
-use crate::claims::Claims;
 
 /// Login request object
 #[derive(Deserialize)]
@@ -21,8 +21,7 @@ pub struct LoginResponse {
 
 /// User authentication, Successful authentication returns a JWT
 #[post("/login", data = "<req>")]
-pub fn login(req: Json<LoginRequest>) ->
-Result<Json<LoginResponse>, Custom<String>> {
+pub fn login(req: Json<LoginRequest>) -> Result<Json<LoginResponse>, Custom<String>> {
     //TODO: use the database
     if req.user != "test" || req.password != "prueba123" {
         return Err(Custom(Status::Unauthorized, "Missing account".to_string()));
